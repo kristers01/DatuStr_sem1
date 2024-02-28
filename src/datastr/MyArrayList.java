7ab4515e9b4dd337ab97613657f8ff203cefb963
Kristers Rukmanis <s22rukmkris@vea.lv>
2024-02-28 09:48:21 +0200

    AAAAUUUUUUUUUUUGGGGGGGHHHHHHHHHHHHHHHHHH

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -2,4 +2,37 @@ package datastr;
 
 public class MyArrayList {
 
+	private int[] list;
+	private final int LIST_SIZE = 10;
+	private int size = LIST_SIZE;
+	private int counter = 0;
+	
+	//TODO
+	//constructors
+	public MyArrayList() {
+		list = new int[size];
+	}
+	
+	public MyarrayList(int inputSize) {
+		if(inputSize > 0) {
+			size = inputSize;
+		}
+		
+		list = new int[size];
+	}
+	
+	//isEmpty
+	public boolean isEmpty() {
+		return counter==0;
+	}
+	
+	//isFull
+	public boolean isFull() {
+		return counter==size;
+	}
+	//howManyElements
+	public int howManyElements() {
+		return counter;
+	}
+	
 }
